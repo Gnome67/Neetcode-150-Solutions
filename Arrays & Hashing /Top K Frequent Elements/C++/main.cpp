5979489de9149615df8ed3b11345131226a86450
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-05-27 13:30:07 -0500

    Update main.cpp

--- Arrays & Hashing /Top K Frequent Elements/C++/main.cpp
+++ Arrays & Hashing /Top K Frequent Elements/C++/main.cpp
@@ -1,1 +1,20 @@
-
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k)
+    {
+        unordered_map<int, int> u;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        { u[nums.at(i)]++; }
+        vector<pair<int, int>> v(u.begin(), u.end());
+        sort(v.begin(), v.end(), [](pair<int, int> x, pair<int, int> y)
+        {
+            if (x.second == y.second) { return x.first < y.first; }
+            else { return x.second > y.second; }
+        } );
+        vector<int> r;
+        for (int i = 0; i < k; i++) { r.push_back(v[i].first); }
+        return r;
+    }
+};
