bcb3eebebb0a0eeb0137218ae68064a269f8e8af
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-05-27 13:25:56 -0500

    Update main.cpp

--- Arrays & Hashing /Longest Consecutive Sequence/C++/main.cpp
+++ Arrays & Hashing /Longest Consecutive Sequence/C++/main.cpp
@@ -1,4 +1,7 @@
-// if(nums.empty()) { return 0; }
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         if(nums.empty()) { return 0; }
 //         if(nums.size() == 1) { return 1; }
 //         int answer = 0, min = nums[nums.size()-1], sequence = 0;
 //         for(int x = 0; x < nums.size(); x++)
@@ -19,3 +22,22 @@
 //         }
 //         return answer;
 //     }
+// };
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>s(nums.begin(), nums.end());
+        int longest = 0;
+        for(auto &n: s){
+            if(!s.count(n - 1)){
+                int length = 1; 
+                while(s.count(n + length))
+                    ++length;
+                longest = max(longest, length);
+            } 
+
+        }
+        return longest;
+    }
+};
