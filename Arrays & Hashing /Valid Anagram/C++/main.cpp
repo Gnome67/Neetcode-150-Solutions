3da12b1008a6318c0e0d1c0048455cfd279942e0
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-05-27 13:32:41 -0500

    Update main.cpp

--- Arrays & Hashing /Valid Anagram/C++/main.cpp
+++ Arrays & Hashing /Valid Anagram/C++/main.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sum;
+        for(int x = 0; x < nums.size(); x++)
+        {
+            for(int y = x + 1; y < nums.size(); y++)
+            {
+                if(nums.at(x) + nums.at(y) == target)
+                {
+                    sum.push_back(x);
+                    sum.push_back(y);
+                    return sum;
+                }
+            }
+        }
+        return sum;
+    }
+};
