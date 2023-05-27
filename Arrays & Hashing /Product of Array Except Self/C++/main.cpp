cf601cf4dd484e7389cc791362e4a8c7fd77a3af
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-05-27 13:29:37 -0500

    Update main.cpp

--- Arrays & Hashing /Product of Array Except Self/C++/main.cpp
+++ Arrays & Hashing /Product of Array Except Self/C++/main.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> answer;
+        if(nums.size() == 0 || nums.size() == 1) { return nums; }
+        int* left = new int[sizeof(int) * nums.size()];
+        int* right = new int[sizeof(int) * nums.size()];
+        int i, j;
+        left[0] = 1;
+        right[nums.size() - 1] = 1;
+        for(i = 1; i < nums.size(); i++) { left[i] = nums[i - 1] * left[i - 1]; }
+        for (j = nums.size() - 2; j >= 0; j--) { right[j] = nums[j + 1] * right[j + 1]; }
+        for (i = 0; i < nums.size(); i++) { answer.push_back(left[i] * right[i]); }
+        return answer;
+    }
+};
