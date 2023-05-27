0aca237472835cca59fa5004c7828c9c75b6bfd3
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-05-27 13:30:49 -0500

    Update main.cpp

--- Arrays & Hashing /Group Anagrams/C++/main.cpp
+++ Arrays & Hashing /Group Anagrams/C++/main.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> groupedAnagrams;
+        unordered_map<string, vector<string>> m;
+        for(int x = 0; x < strs.size(); x++)
+        {
+            string y = strs[x];
+            sort(y.begin(), y.end());
+            m[y].push_back(strs[x]);
+        }
+        for(auto z:m)
+        {
+            groupedAnagrams.push_back(z.second);
+        }
+        return groupedAnagrams;
+    }
+};
