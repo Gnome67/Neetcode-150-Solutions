cadc13d1e41f7a59d087d2acae32020a69f88620
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-05-27 13:24:04 -0500

    Update main.cpp

--- Arrays & Hashing /Longest Consecutive Sequence/C++/main.cpp
+++ Arrays & Hashing /Longest Consecutive Sequence/C++/main.cpp
@@ -1,1 +1,21 @@
-
+// if(nums.empty()) { return 0; }
+//         if(nums.size() == 1) { return 1; }
+//         int answer = 0, min = nums[nums.size()-1], sequence = 0;
+//         for(int x = 0; x < nums.size(); x++)
+//         {
+//             if(nums[x] < min) { min = nums[x]; }
+//         }
+//         answer++;
+//         int middle = min+1;
+//         while(sequence < nums.size())
+//         {
+//             if(nums[sequence] == middle)
+//             {
+//                 answer++;
+//                 middle++;
+//                 sequence = 0;
+//             }
+//             else { sequence++; }
+//         }
+//         return answer;
+//     }
