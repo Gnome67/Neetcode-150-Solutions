73e2da9e018e0754d4d61714346e348b0f801ad1
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-05-27 13:32:12 -0500

    Update main.cpp

--- Arrays & Hashing /Two Sum/C++/main.cpp
+++ Arrays & Hashing /Two Sum/C++/main.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int lenS = s.length();
+        int lenT = t.length();
+        if(lenS != lenT) {return false; }
+        unordered_map <char,int> anagram;
+        for(int x = 0; x < lenS; x++)
+        {
+            anagram[s[x]]++;
+            anagram[t[x]]--;
+        }
+        for(auto y:anagram) {
+            if(y.second) { return false; }
+        }
+        return true;
+    }
+};
