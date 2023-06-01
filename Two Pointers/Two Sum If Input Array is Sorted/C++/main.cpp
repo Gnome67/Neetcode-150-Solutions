2eff525ea7d2c07d98c74d64f9161f49180321cd
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-06-01 12:49:51 -0500

    Update main.cpp

--- Two Pointers/Two Sum If Input Array is Sorted/C++/main.cpp
+++ Two Pointers/Two Sum If Input Array is Sorted/C++/main.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0, j = numbers.size()-1;
+        while(i<j)
+        {
+            int sum=numbers[i]+numbers[j];
+            if(sum<target) { i++; }
+            else if(sum>target) { j--; }
+            else { return vector{i+1, j+1}; }
+        }
+        return vector{-1, -1};
+        // for(int x = 0; x < numbers.size(); x++)
+        // {
+        //     for(int y = x + 1; y < numbers.size(); y++)
+        //     {
+        //         if(numbers[x] + numbers[y] == target)
+        //         {
+        //             answer.push_back(x+1);
+        //             answer.push_back(y+1);
+        //             return answer;
+        //         }
+        //     }
+        // }
+    }
+};
