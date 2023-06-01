3e15add3191f98d42492acb93d9b5555cbb0a362
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-06-01 12:49:03 -0500

    Update main.cpp

--- Two Pointers/3Sum/C++/main.cpp
+++ Two Pointers/3Sum/C++/main.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> answer;
+        if(nums.size() < 3) { return answer; }
+        vector<int> myVector;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 2; i++)
+        {
+            if(nums[i] > 0) { break; }
+            if(i > 0 && nums[i - 1] == nums[i]) { continue; }
+            int j = i + 1, k = nums.size() - 1;
+            while(j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0) { j++; }
+                else if(sum > 0) { k--; }
+                else
+                {
+                    answer.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1]) { j++; }
+                    j++;
+                    while (j < k && nums[k - 1] == nums[k]) { k--; }
+                    k--;
+                }
+            }
+        }
+        return answer;
+    }
+};
