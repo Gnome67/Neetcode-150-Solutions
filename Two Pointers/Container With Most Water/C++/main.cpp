e78fb26d62eb83f121f3625b25d9b8379229feab
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-06-01 12:49:15 -0500

    Update main.cpp

--- Two Pointers/Container With Most Water/C++/main.cpp
+++ Two Pointers/Container With Most Water/C++/main.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int answer = 0, l = 0, r = height.size()-1, maxL = height[l], maxR = height[r];
+        while(l < r)
+        {
+            if(maxL < maxR)
+            {
+                l++;
+                if(height[l] > maxL) { maxL = height[l]; }
+                answer = answer + (maxL - height[l]);
+            }
+            else
+            {
+                r--;
+                if(height[r] > maxR) { maxR = height[r]; }
+                answer = answer + (maxR - height[r]);
+            }
+        }
+        return answer;
+    }
+};
