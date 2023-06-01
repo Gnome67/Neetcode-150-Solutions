ddc039da2b8b2a28b65b19d9e0a8df3566912b7a
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-06-01 12:48:22 -0500

    Update main.cpp

--- Two Pointers/Valid Palindrome/C++/main.cpp
+++ Two Pointers/Valid Palindrome/C++/main.cpp
@@ -1,1 +1,44 @@
-
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.length() == 0 || s.length() == 1) { return true; }
+        s.erase(remove(s.begin(), s.end(), ' '), s.end());
+        s.erase(remove(s.begin(), s.end(), '.'), s.end());
+        s.erase(remove(s.begin(), s.end(), ','), s.end());
+        s.erase(remove(s.begin(), s.end(), ':'), s.end());
+        s.erase(remove(s.begin(), s.end(), '!'), s.end());
+        s.erase(remove(s.begin(), s.end(), '@'), s.end());
+        s.erase(remove(s.begin(), s.end(), '#'), s.end());
+        s.erase(remove(s.begin(), s.end(), '_'), s.end());
+        s.erase(remove(s.begin(), s.end(), '{'), s.end());
+        s.erase(remove(s.begin(), s.end(), '}'), s.end());
+        s.erase(remove(s.begin(), s.end(), '\''), s.end());
+        s.erase(remove(s.begin(), s.end(), '"'), s.end());
+        s.erase(remove(s.begin(), s.end(), '['), s.end());
+        s.erase(remove(s.begin(), s.end(), ']'), s.end());
+        s.erase(remove(s.begin(), s.end(), '-'), s.end());
+        s.erase(remove(s.begin(), s.end(), '?'), s.end());
+        s.erase(remove(s.begin(), s.end(), ';'), s.end());
+        s.erase(remove(s.begin(), s.end(), '&'), s.end());
+        s.erase(remove(s.begin(), s.end(), '('), s.end());
+        s.erase(remove(s.begin(), s.end(), ')'), s.end());
+        s.erase(remove(s.begin(), s.end(), '`'), s.end());
+        //add any remaining characters to be removed
+        if(s.length() == 0 || s.length() == 1) { return true; }
+        bool answer = true;
+        for (int x = 1; x < s.length(); x++)
+        {
+            cout << "Front: " << char(tolower(s[x-1])) << endl << "Back: " << char(tolower(s[s.length()-x])) << endl;
+            if(char(tolower(s[x-1])) == char(tolower(s[s.length()-x])) && isalnum(char(tolower(s[x-1]))) && isalnum(char(tolower(s[s.length()-x]))))
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+};
