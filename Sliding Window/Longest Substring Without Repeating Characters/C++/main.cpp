92cbe00400e182fc4cf4e53d47bb23163bfc3186
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-07-12 10:56:13 -0500

    Update main.cpp

--- Sliding Window/Longest Substring Without Repeating Characters/C++/main.cpp
+++ Sliding Window/Longest Substring Without Repeating Characters/C++/main.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> answer;
+        int maxSize = 0, left = 0, right = 0;
+        while(right < s.size())
+        {
+            while(answer.find(s[right]) != answer.end())
+            {
+                answer.erase(s[left]);
+                left++;
+            }
+            maxSize = max(maxSize, right - left + 1);
+            answer.insert(s[right]);
+            right++;
+        }
+        return maxSize;
+    }
+};
