2f6a0645241920c6062fffdd98b1b2bef01d5b3c
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-07-17 12:07:26 -0500

    Update main.cpp

--- Stack/Daily Temperatures/C++/main.cpp
+++ Stack/Daily Temperatures/C++/main.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<pair<int, int>> stk;
+        vector<int> answer(n);
+        for(int i = 0; i < n; i++)
+        {
+            int currentDay = i;
+            int currentTemp = temperatures[i];
 
+            while(!stk.empty() && stk.top().second < currentTemp)
+            {
+                int previousDay = stk.top().first;
+                int previousTemp = stk.top().second;
+                stk.pop();
+                answer[previousDay] = currentDay - previousDay;
+            }
+            stk.push({currentDay, currentTemp});
+        }
+        return answer;
+    }
+};
