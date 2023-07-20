bb7fdf27c497db75b022da34f241c786a1e8ba1a
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-07-20 11:15:48 -0500

    Update main.py

--- Two Pointers/Container With Most Water/Python/main.py
+++ Two Pointers/Container With Most Water/Python/main.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r, res, h = 0, len(height) - 1, 0, max(height)
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+            if(r-l) * h <= res:
+                break
+        return res
