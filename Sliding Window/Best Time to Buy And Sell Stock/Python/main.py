7e8c0a853edb186735111a3bc861e05ad2d81c67
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-07-22 12:01:38 -0500

    Update main.py

--- Sliding Window/Best Time to Buy And Sell Stock/Python/main.py
+++ Sliding Window/Best Time to Buy And Sell Stock/Python/main.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res, lowest = 0, prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            res = max(res, price - lowest)
+        return res
