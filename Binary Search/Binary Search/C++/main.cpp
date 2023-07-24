bea16851d6cbf3c41f94cd578209fd40aa0f26cf
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-07-24 12:17:49 -0500

    Update main.cpp

--- Binary Search/Binary Search/C++/main.cpp
+++ Binary Search/Binary Search/C++/main.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // int n = nums.size();
+        // int x = ceil((n)/2);
+        // while(x != n)
+        //    if(nums[x] == target) { return target; }
+        //    if(nums[x] < target) { x++; }
+        //    if(nums[x] > target) { x--; }
+        // }
+        int low = 0, high = nums.size() - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) { low = mid + 1; }
+            else if (nums[mid] > target) { high = mid - 1; }
+            else { return mid; }
+        }
+        return -1;
+    }
+};
