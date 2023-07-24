fcef9ad0751e0e9b9a0ad6e3ef80965610b4e921
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-07-24 12:28:28 -0500

    Update main.cpp

--- Binary Search/Koko Eating Bananas/C++/main.cpp
+++ Binary Search/Koko Eating Bananas/C++/main.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1, r = 0;
+        for(int x = 0; x < piles.size(); x++) { r = max(r, piles[x]); }
+        int answer = r;
+        while(l <= r)
+        {
+            int k = l + (r - l) / 2;
+            long int hours = 0;
+            for(int i = 0; i < piles.size(); i++) { hours += ceil((double) piles[i] / k); }
+            if(hours <= h)
+            {
+                answer = min(answer, k);
+                r = k - 1;
+            } else { l = k + 1; }
+        }
+        return answer;
+    }
+};
