e0a39918d912df031db792d91eee02cff3146a0f
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-07-24 12:28:44 -0500

    Update main.cpp

--- Binary Search/Find Minimum In Rotated Sorted Array/C++/main.cpp
+++ Binary Search/Find Minimum In Rotated Sorted Array/C++/main.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size(), min = INT_MAX;
+        for(int x = 0; x < nums.size(); x++)
+        {
+            if(nums[x] < min) { min = nums[x]; }
+        }
+        return min;
+    }
+};
