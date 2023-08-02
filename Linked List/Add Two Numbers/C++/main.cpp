dcaf43e74a405a5d94e29a25e6caa8279dd91192
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-08-02 12:44:56 -0500

    Update main.cpp

--- Linked List/Add Two Numbers/C++/main.cpp
+++ Linked List/Add Two Numbers/C++/main.cpp
@@ -1,1 +1,31 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* temp = new ListNode();
+        ListNode* curr = temp;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL)
+        {
+            int val1 = (l1 != NULL) ? l1->val : 0;
+            int val2 = (l2 != NULL) ? l2->val : 0;    
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            if (l1 != NULL) { l1 = l1->next; }
+            if (l2 != NULL) { l2 = l2->next; }
+        }
+        if (carry == 1) { curr->next = new ListNode(1); }
+        return temp->next;
+    }
+};
