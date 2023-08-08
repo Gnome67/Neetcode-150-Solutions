60662067bc8ba15d9a47b00e815ea4f26fd9db40
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-08-08 12:06:55 -0500

    Update main.cpp

--- Binary Search/Search in Rotated Sorted Array/C++/main.cpp
+++ Binary Search/Search in Rotated Sorted Array/C++/main.cpp
@@ -1,1 +1,8 @@
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int x = 0; x < n; x++) { if(nums[x] == target) { return x; } }
+        return -1;
+    }
+};
