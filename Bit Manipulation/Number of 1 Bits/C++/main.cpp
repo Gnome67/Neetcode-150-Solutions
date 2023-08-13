9f20f2c146d70457e36ecb67a8fb575637b510e3
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-08-13 10:41:54 -0500

    Update main.cpp

--- Bit Manipulation/Number of 1 Bits/C++/main.cpp
+++ Bit Manipulation/Number of 1 Bits/C++/main.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int bit = 0, result = 0;
+        while (n != 0) {
+            bit = n & 1;
+            if (bit == 1) { result++; }
+            n = n >> 1;
+        }
+        return result;
+    }
+};
